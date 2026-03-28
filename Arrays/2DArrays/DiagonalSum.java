69ade945f83f97c72883ca1e351fcf6541e514d4
Aditya Kumar Rana <ranaaditya2904@gmail.com>
2026-03-28 11:19:54 +0530

    Diagonal Sum

---
+++ Arrays/2DArrays/DiagonalSum.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class DiagonalSum {
+    public static int Dsum(int arr[][]){
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+                sum=sum+arr[i][i];
+                if(i!=arr.length-1)
+                    sum+=arr[i][arr.length-i-1];
+            }
+            return sum;
+        }
+    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter rows and cols");
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+
+        int arr[][]=new int[n][m];
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+
+        int result=Dsum(arr);
+        System.out.println("THe sum is: "+result);
+
+    }
+}
